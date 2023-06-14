dbdf45fd019faa12ec42db22c4c7d2c20f03d825
MembriniLuca29 <lucamembrini29@gmail.com>
2023-06-14 14:47:25 +0200

    bonus 2 completato

---
+++ bonus 2/js/script.js
@@ -0,0 +1,68 @@
+
+
+
+
+const container = document.querySelector('.container');
+for (let numero = 1; numero <= 100; numero++) {
+    
+let valore = numero
+
+if (numero % 5 == 0 && numero % 3 == 0) {
+    valore = "FizzBuzz"
+}
+if (numero % 3 == 0 && numero % 5 != 0) {
+    valore = "Fizz"
+}
+
+if (numero % 5 == 0 && numero % 3 != 0) {
+    valore = "Buzz"
+}
+if (numero % 5 != 0 && numero % 3 != 0) {
+    valore = numero
+}
+
+    const newLi = document.createElement('div');
+    newLi.innerHTML = valore;
+    container.append(newLi);
+
+    if (numero % 5 == 0 && numero % 3 == 0) {
+        newLi.style.backgroundColor="#F0466F";
+    }
+    if (numero % 3 == 0 && numero % 5 != 0) {
+        newLi.style.backgroundColor="#0CD6A1";
+    }
+    
+    if (numero % 5 == 0 && numero % 3 != 0) {
+        newLi.style.backgroundColor="#FFD166";
+    }
+    if (numero % 5 != 0 && numero % 3 != 0) {
+        newLi.style.backgroundColor="#1389B2";
+    }
+
+}
+
+/*const ul = document.querySelector('.list');
+for (let numero = 1; numero <= 100; numero++) {
+    
+let valore = numero
+
+if (numero % 5 == 0 && numero % 3 == 0) {
+    valore = "FizzBuzz"
+}
+if (numero % 3 == 0 && numero % 5 != 0) {
+    valore = "Fizz"
+}
+
+if (numero % 5 == 0 && numero % 3 != 0) {
+    valore = "Buzz"
+}
+if (numero % 5 != 0 && numero % 3 != 0) {
+    valore = numero
+}
+
+    const newLi = document.createElement('li');
+    newLi.innerHTML = valore;
+    ul.append(newLi);
+
+}*/
+
