46aaced001213d365d6b6062fab5bc6ff7f62691
MembriniLuca29 <lucamembrini29@gmail.com>
2023-06-14 14:52:54 +0200

    tolte le cose inutili

--- bonus 2/js/script.js
+++ bonus 2/js/script.js
@@ -1,7 +1,4 @@
 
-
-
-
 const container = document.querySelector('.container');
 for (let numero = 1; numero <= 100; numero++) {
     
@@ -40,29 +37,4 @@ if (numero % 5 != 0 && numero % 3 != 0) {
     }
 
 }
-
-/*const ul = document.querySelector('.list');
-for (let numero = 1; numero <= 100; numero++) {
-    
-let valore = numero
-
-if (numero % 5 == 0 && numero % 3 == 0) {
-    valore = "FizzBuzz"
-}
-if (numero % 3 == 0 && numero % 5 != 0) {
-    valore = "Fizz"
-}
-
-if (numero % 5 == 0 && numero % 3 != 0) {
-    valore = "Buzz"
-}
-if (numero % 5 != 0 && numero % 3 != 0) {
-    valore = numero
-}
-
-    const newLi = document.createElement('li');
-    newLi.innerHTML = valore;
-    ul.append(newLi);
-
-}*/
 
